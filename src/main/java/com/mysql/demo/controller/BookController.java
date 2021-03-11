a23eb3f4df38d0e87f4d2acc6cc5a4851479027d
ekantgupta <ekantgupta@gmail.com>
2021-03-11 15:30:59 +0530

    Unused imports removed

--- src/main/java/com/mysql/demo/controller/BookController.java
+++ src/main/java/com/mysql/demo/controller/BookController.java
@@ -4,12 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
